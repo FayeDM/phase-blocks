ed759eeac3980cccb6b24d8c0fc1e64113ce69c9
Faye Polson <faye@kanopistudios.com>
2026-02-25 13:55:36 -0700

    Delete phase-blocks directory

--- phase-blocks/cta-block/src/index.js
+++
@@ -1,9 +0,0 @@
-import Edit from './edit';
-
-import { registerBlockType } from '@wordpress/blocks';
-import metadata from '../block.json';
-
-registerBlockType(metadata.name, {
-    edit: Edit,
-    save: () => null, // Server-side render, if you have render.php
-});
